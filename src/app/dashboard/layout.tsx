447bc33e0c467aa51e868a8d4c6465b5cd7d775a
Assaf Klieger <klieger.a@gmail.com>
2026-02-16 07:16:25 +0000

    Stil endless scrolls

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -13,17 +13,15 @@ export default function DashboardLayout({
 }) {
   return (
     <SidebarProvider>
-      <div className="min-h-screen bg-background">
-        <Sidebar side="left" collapsible="icon">
-          <SidebarNav />
-        </Sidebar>
-        <SidebarInset>
-          <Header />
-          <div className="flex-1 overflow-y-auto p-4 sm:p-6 lg:p-8">
-            <div className="mx-auto w-full">{children}</div>
-          </div>
-        </SidebarInset>
-      </div>
+      <Sidebar side="left" collapsible="icon">
+        <SidebarNav />
+      </Sidebar>
+      <SidebarInset>
+        <Header />
+        <div className="flex-1 overflow-y-auto p-4 sm:p-6 lg:p-8">
+          <div className="mx-auto w-full max-w-7xl">{children}</div>
+        </div>
+      </SidebarInset>
     </SidebarProvider>
   );
 }
