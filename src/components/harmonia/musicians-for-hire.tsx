6af55a4503affd3ccaa39c03b4389a8559790771
Assaf Klieger <klieger.a@gmail.com>
2026-02-20 08:04:03 +0000

    Try fixing this error: `Runtime ReferenceError: CardFooter is not define

--- src/components/harmonia/musicians-for-hire.tsx
+++ src/components/harmonia/musicians-for-hire.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useState, useMemo } from "react";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from "@/components/ui/card";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { PlaceHolderImages } from '@/lib/placeholder-images';
 import Image from 'next/image';
