1354156ea874d024e3d88464f4203419c52f5ccc
Assaf Klieger <klieger.a@gmail.com>
2026-02-27 16:24:00 +0000

    translations

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -2,6 +2,7 @@ import { getRequestConfig } from 'next-intl/server';
 import { routing } from './routing';
 
 export default getRequestConfig(async ({ locale }) => {
+    // Validate that the incoming `locale` parameter is valid
     if (!routing.locales.includes(locale as any)) {
         locale = routing.defaultLocale;
     }
@@ -9,19 +10,19 @@ export default getRequestConfig(async ({ locale }) => {
     let messages;
     switch (locale) {
         case 'en':
-            messages = (await import('../../messages/en.json')).default;
+            messages = (await import('../messages/en.json')).default;
             break;
         case 'ar':
-            messages = (await import('../../messages/ar.json')).default;
+            messages = (await import('../messages/ar.json')).default;
             break;
         case 'he':
-            messages = (await import('../../messages/he.json')).default;
+            messages = (await import('../messages/he.json')).default;
             break;
         case 'ru':
-            messages = (await import('../../messages/ru.json')).default;
+            messages = (await import('../messages/ru.json')).default;
             break;
         default:
-            messages = (await import(`../../messages/he.json`)).default;
+            messages = (await import('../messages/he.json')).default;
     }
 
     return {
